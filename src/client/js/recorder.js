bf1954621e2ae7d75e719db67e449db9e23d98b3
azerckid <azerckid@gmail.com>
2025-06-19 15:07:13 +0900

    #13.2 Recording Video

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,15 +1,43 @@
 const startBtn = document.getElementById("startBtn");
 const preview = document.getElementById("preview");
 
-const handleStart = async () => {
-    console.log("start recording");
-    const stream = await navigator.mediaDevices.getUserMedia({
-        audio: true,
+
+let stream;
+
+const handleStop = () => {
+    startBtn.innerText = "Start Recording";
+    startBtn.removeEventListener("click", handleStop);
+    startBtn.addEventListener("click", handleStart);
+};
+
+const handleStart = () => {
+    startBtn.innerText = "Stop Recording";
+    startBtn.removeEventListener("click", handleStart);
+    startBtn.addEventListener("click", handleStop);
+    const recorder = new MediaRecorder(stream);
+    recorder.ondataavailable = (e) => {
+        console.log("recording done");
+        console.log(e);
+        console.log(e.data);
+    };
+    console.log(recorder);
+    recorder.start();
+    console.log(recorder);
+    setTimeout(() => {
+        recorder.stop();
+    }, 5000);
+};
+
+const init = async () => {
+    stream = await navigator.mediaDevices.getUserMedia({
+        audio: false,
         video: true,
     });
     console.log(stream);
     preview.srcObject = stream;
     preview.play();
 };
+
+init();
 
 startBtn.addEventListener("click", handleStart);
